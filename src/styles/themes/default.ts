ead4326f61172f0506a78a247056dd487a225632
antonioRenato <antoniorpfs@gmail.com>
2023-06-08 17:16:17 -0300

    add global css

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  '--yellow-700': '#C47F17',
+  '--yellow-500': '#DBAC2C',
+  '--yellow-300': '#F1E9C9',
+
+  '--purple-700': '#4B2995',
+  '--purple-500': '#8047F8',
+  '--purple-300': '#EBE5F9',
+
+  '--gray-900': '#272221',
+  '--gray-700': '#403937',
+  '--gray-500': '#574F4D',
+  '--gray-300': '#8D8686',
+  '--gray-200': '#D7D5D5',
+
+  '--white-600': '#E6E5E5',
+  '--white-500': '#EDEDED',
+  '--white-400': '#F3F2F2',
+  '--white-300': '#FAFAFA',
+}
