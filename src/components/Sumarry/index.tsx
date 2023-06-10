fd959778c33e4b8a297e2f3f2207a1cfb2e815bf
antonioRenato <antoniorpfs@gmail.com>
2023-06-09 21:40:47 -0300

    add icons

--- src/components/Sumarry/index.tsx
+++ src/components/Sumarry/index.tsx
@@ -1,4 +1,10 @@
 import coffeeLogo from '../../assets/CoffeLogo.svg'
+
+import BoxIcon from '../../assets/icons/BoxIcon.svg'
+import CoffeeIcon from '../../assets/icons/CoffeeIcon.svg'
+import ShoppingCartIcon from '../../assets/icons/ShoppingCartIcon.svg'
+import TimerIcon from '../../assets/icons/TimerIcon.svg'
+
 import { CoffeeLogo, SummaryBackground, SummaryText } from './styles'
 
 export function Summary() {
@@ -12,9 +18,13 @@ export function Summary() {
             hora
           </p>
           <div>
+            <img src={BoxIcon} alt="" />
             <p>Compra simples e segura</p>
+            <img src={CoffeeIcon} alt="" />
             <p>Embalagem mantém o café intacto</p>
+            <img src={ShoppingCartIcon} alt="" />
             <p>Entrega rápida e rastreada</p>
+            <img src={TimerIcon} alt="" />
             <p>O café chega fresquinho até você</p>
           </div>
         </SummaryText>
