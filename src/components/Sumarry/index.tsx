f5879834490d527823b1000a63ebcd5e214aafc6
antonioRenato <antoniorpfs@gmail.com>
2023-06-10 21:35:49 -0300

    create item

--- src/components/Sumarry/index.tsx
+++ src/components/Sumarry/index.tsx
@@ -5,10 +5,14 @@ import CoffeeIcon from '../../assets/icons/CoffeeIcon.svg'
 import ShoppingCartIcon from '../../assets/icons/ShoppingCartIcon.svg'
 import TimerIcon from '../../assets/icons/TimerIcon.svg'
 
+import ExpressoImg from '../../assets/expresso.svg'
+
 import {
   CoffeeLogo,
+  MainItem,
   SummaryBackground,
   SummaryIcons,
+  SummaryMain,
   SummaryText,
 } from './styles'
 
@@ -37,12 +41,24 @@ export function Summary() {
           <img src={coffeeLogo} alt="" />
         </CoffeeLogo>
       </SummaryBackground>
-      <div>
+      <SummaryMain>
         <span>Nossos cafés</span>
-        <div>
-          <div></div>
-        </div>
-      </div>
+
+        <MainItem>
+          <img src={ExpressoImg} alt="" />
+          <p className="tag">TRADICIONAL</p>
+          <span>Expresso Tradicional</span>
+          <p>O tradicional café feito com água quente e grãos moídos</p>
+          <div className="price">
+            <span>R$ 9,90</span>
+            <div>
+              <button>-</button>
+              <p>1</p>
+              <button>+</button>
+            </div>
+          </div>
+        </MainItem>
+      </SummaryMain>
     </>
   )
 }
--- src/components/Sumarry/styles.ts
+++ src/components/Sumarry/styles.ts
@@ -66,3 +66,133 @@ export const SummaryIcons = styled.div`
     width: 2rem; /* Preenche a largura da coluna */
   }
 `
+
+export const SummaryMain = styled.main`
+  margin: auto 12rem;
+  margin-top: 2rem;
+
+  > span {
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 130%;
+  }
+`
+
+export const MainItem = styled.div`
+  width: 16rem;
+  height: 19.375rem;
+  border-radius: 8px;
+
+  background-color: ${(props) => props.theme['--white-400']};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+
+  img {
+    margin-top: -4rem;
+  }
+
+  .tag {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    margin-top: 0.4rem;
+    font-weight: 700;
+    font-size: 10px;
+    font-weight: bold;
+    line-height: 130%;
+
+    border-radius: 999px;
+    height: 1.2rem;
+    width: 4.6rem;
+    /* identical to box height, or 13px */
+    /* Brand / Yellow Dark */
+    color: ${(props) => props.theme['--yellow-700']};
+    background-color: ${(props) => props.theme['--yellow-300']};
+  }
+
+  > span {
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 130%;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 130%;
+    /* or 18px */
+
+    text-align: center;
+    color: ${(props) => props.theme['--gray-300']};
+
+    margin: 0 0.5rem;
+  }
+
+  .price {
+    width: 100%;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    gap: 2rem;
+
+    span {
+      font-weight: 700;
+      font-size: 20px;
+      line-height: 130%;
+      /* or 31px */
+
+      text-align: center;
+      /* Base/Text */
+      color: ${(props) => props.theme['--gray-500']};
+    }
+
+    div {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      width: 4.5rem;
+      height: 2.4rem;
+
+      background: ${(props) => props.theme['--white-600']};
+      border-radius: 6px;
+
+      p {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 130%;
+
+        color: ${(props) => props.theme['--gray-900']};
+      }
+
+      button {
+        color: ${(props) => props.theme['--purple-500']};
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 140%;
+
+        background: ${(props) => props.theme['--white-600']};
+
+        width: 2rem;
+        border: 0;
+
+        cursor: pointer;
+      }
+    }
+  }
+`
